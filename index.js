c98b44b77b75b830a1605898c4a7d89aaeef1a81
Andrew Lee <13341633+DW225@users.noreply.github.com>
2021-12-21 13:13:23 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// [START functions_helloworld_pubsub]
+/**
+ * Background Cloud Function to be triggered by Pub/Sub.
+ * This function is exported by index.js, and executed when
+ * the trigger topic receives a message.
+ *
+ * @param {object} message The Pub/Sub message.
+ * @param {object} context The event metadata.
+ */
+exports.helloPubSub = (message, context) => {
+  const name = message.data
+    ? Buffer.from(message.data, 'base64').toString()
+    : 'World';
+
+  console.log(`Hello, ${name}!`);
+};
+// [END functions_helloworld_pubsub]
